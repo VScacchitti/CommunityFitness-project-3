304cf4026ab39fbef36dc34f25420c7c76c61f9b
Vince Scacchitti <VScacchitti24@gmail.com>
2021-01-14 15:36:02 -0500

    added component for add weight, get and post routes work.

---
+++ client/src/components/WeightForm.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import weightAPI from "../utils/weight/API"
+
+
+class WeightForm extends Component {
+
+    state = {
+        weight: 0,
+        units:"lbs",
+        error: "",
+      };
+ //THhese handle our input changes for Weight, and if they change lbs to another metric.
+      handleChangeWeight = (event) => {
+        this.setState({
+            weight: event.target.value
+        });
+      }
+
+      handleChangeUnit = (event) => {
+        this.setState({
+            units: event.target.value
+        });
+      }
+
+    //function to handle form submit
+    handleFormSubmit = event => {
+        event.preventDefault();
+        let savedWeight = {
+            weight: this.state.weight,
+            units: this.state.units,
+            
+        }
+        weightAPI.saveWeight(savedWeight)
+        .then(console.log(savedWeight))
+        .then(alert("You added a weight"))
+        .catch(err => console.log(err));   
+    }
+
+
+  render(){
+    return (
+        <div className="workout-form">
+            <h2>Add or Update Weight!</h2>
+          <form >
+            <input value={this.state.value} type="text" placeholder="Weight" onChange={this.handleChangeWeight} />
+            <input value={this.state.value} type="text" placeholder="lbs" onChange={this.handleChangeUnit} />
+          </form>
+          <button type="submit" id="weight Submit" onClick={this.handleFormSubmit}>Add Weight!</button>
+        </div>
+      )
+
+  }
+
+}
+
+export default WeightForm;
--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,13 @@ const router = require("express").Router();
 const apiRoutes1 = require("./api/exerciseroutes");
 const apiRoutes2 = require("./api/recordroutes");
 const apiRoutes3 = require("./api/workoutroutes");
+const apiRoutes4 = require("./api/weightroutes");
 
 // API Routes
 router.use("/api", apiRoutes1);
 router.use("/api", apiRoutes2);
 router.use("/api", apiRoutes3);
+router.use("/api", apiRoutes4);
 
 // If no API routes are hit, send the React app
 router.use(function(req, res) {
