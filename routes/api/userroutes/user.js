5b2540680306c46cc3a0793804efccf4f7a1f14f
brennandoehring <nannerbdale@gmail.com>
2021-01-19 10:23:53 -0500

    Token authentication and delete user added

--- routes/api/userroutes/user.js
+++ routes/api/userroutes/user.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const auth = require("../middleware/auth");
 const User = require("../../../models/user/user");
 const userController = require("../../../controllers/userController");
 
@@ -77,6 +78,34 @@ router.post("/login", async (req, res) => {
                 email: user.email,
             },
         });
+    } catch (err) {
+        res.status(500).json({ error: err.message});
+    }
+});
+
+// Delete user
+router.delete("/delete", auth, async (req, res) => {
+    try {
+        const deletedUser = await User.findByIdAndDelete(req.user);
+        res.json(deletedUser);
+    } catch (err) {
+        res.status(500).json({ error: err.message});
+    }
+});
+
+// Token validation
+router.post("/tokenIsValid", async (req, res) => {
+    try {
+        const token = req.header("x-auth-token");
+        if (!token) return res.json(false);
+
+        const verified = jwt.verify(token, process.env.JWT_TOKEN);
+        if (!verified) return res.json(false);
+
+        const user = await User.findById(verified.id);
+        if (!user) return res.json(false);
+
+        return res.json(true);
     } catch (err) {
         res.status(500).json({ error: err.message});
     }
